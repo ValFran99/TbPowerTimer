82ba31e3bfa6685d7bed52145eb2680477149099
ValFran99 <72045238+ValFran99@users.noreply.github.com>
2023-07-07 16:54:21 -0300

    feat: input only updates when bellow limit

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const TB_RECHARGE_IN_SEG = 360
+const MAX_TB = 180
+
+
+function testValue(){
+  let inputValue = document.getElementById("input-tb").value
+  if(inputValue > MAX_TB){
+    return
+  }
+  document.getElementById("datetime").innerHTML = document.getElementById("input-tb").value
+}
+
 function getDate(){
   var dt = Date()
   document.getElementById("datetime").innerHTML = dt
