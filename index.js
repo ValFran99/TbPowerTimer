a75499799f4d68e58177b8c5f02e7d0703bcb1fe
ValFran99 <72045238+ValFran99@users.noreply.github.com>
2023-07-08 21:54:00 -0300

    made the setInterval a global variable

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const TB_RECHARGE_IN_SEC = 360
 const MAX_TB = 180
-
+var RECHARGE_INTERVAL = 0
 
 function testValue(){
+  clearInterval(RECHARGE_INTERVAL)
   let tbInput = document.getElementById("input-tb").value
   if(tbInput > MAX_TB){
     return
   }
 
-  let rechargeSeconds = tbInput * TB_RECHARGE_IN_SEC
+  var rechargeInSeconds = tbInput * TB_RECHARGE_IN_SEC
 
-  document.getElementById("recharge-time").innerHTML = new Date(rechargeSeconds * 1000).toISOString().slice(11, 19);
+  RECHARGE_INTERVAL = setInterval(function(){
+    if(rechargeInSeconds <= 0){
+      clearInterval(RECHARGE_INTERVAL)
+    }
+    document.getElementById("recharge-time").innerHTML = new Date(rechargeInSeconds * 1000).toISOString().slice(11, 19);
+    rechargeInSeconds -= 1
+  }, 1000)
+  return
 }
 
 function getDate(){
