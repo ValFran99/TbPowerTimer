b3d994c7aea7e4952aac5baa84bcf1e70d2f4ff4
ValFran99 <72045238+ValFran99@users.noreply.github.com>
2023-07-08 21:28:14 -0300

    feat: converts tb power to HH:MM:SS but ugly

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const TB_RECHARGE_IN_SEG = 360
+const TB_RECHARGE_IN_SEC = 360
 const MAX_TB = 180
 
 
 function testValue(){
-  let inputValue = document.getElementById("input-tb").value
-  if(inputValue > MAX_TB){
+  let tbInput = document.getElementById("input-tb").value
+  if(tbInput > MAX_TB){
     return
   }
-  document.getElementById("datetime").innerHTML = document.getElementById("input-tb").value
+
+  // let initialSegundos = 2000
+  // let horas = Math.floor(initialSegundos / 3600)
+  // let segundos = initialSegundos % 3600
+  // let minutes = Math.floor(segundos / 60)
+  // segundos %= 60
+
+  let initialSeconds = tbInput * TB_RECHARGE_IN_SEC
+  let rechargeHour = Math.floor(initialSeconds / 3600)
+  let rechargeSeconds = initialSeconds % 3600
+  let rechargeMinutes = rechargeSeconds / 60
+  rechargeSeconds %= 60
+  document.getElementById("recharge-time").innerHTML = rechargeHour + ":" + rechargeMinutes + ":" + rechargeSeconds
 }
 
 function getDate(){
   var dt = Date()
-  document.getElementById("datetime").innerHTML = dt
+  document.getElementById("recharge-time").innerHTML = dt
 }
