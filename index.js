d5697a42568981c5ecb834af297c73c42105e312
ValFran99 <72045238+ValFran99@users.noreply.github.com>
2023-07-09 13:02:19 -0300

    bad code is ugly, but it works

--- index.js
+++ index.js
@@ -10,15 +10,26 @@ function testValue(){
   }
 
   var rechargeInSeconds = tbInput * TB_RECHARGE_IN_SEC
+  let browserOffset = new Date().getTimezoneOffset() * 60 * 1000
+
+  updateDocument(rechargeInSeconds, browserOffset)
+  rechargeInSeconds -= 1
 
   RECHARGE_INTERVAL = setInterval(function(){
     if(rechargeInSeconds <= 0){
       clearInterval(RECHARGE_INTERVAL)
     }
-    document.getElementById("recharge-time").innerHTML = new Date(rechargeInSeconds * 1000).toISOString().slice(11, 19);
+
+    updateDocument(rechargeInSeconds, browserOffset)
     rechargeInSeconds -= 1
   }, 1000)
   return
+}
+
+function updateDocument(rechargeInSeconds, browserOffset){
+  document.getElementById("page-title").innerHTML = "Trailblaze " + new Date(rechargeInSeconds * 1000).toISOString().slice(11, 19);
+    document.getElementById("recharge-time").innerHTML = new Date(rechargeInSeconds * 1000).toISOString().slice(11, 19);
+    document.getElementById("recharge-date").innerHTML = new Date(rechargeInSeconds * 1000 + Date.now() - browserOffset).toISOString().slice(11, 16) + " HS";
 }
 
 function getDate(){
