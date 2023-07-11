02a04263d2fb07580a27ae7a83659049b6730766
ValFran99 <72045238+ValFran99@users.noreply.github.com>
2023-07-11 16:57:53 -0300

    fix: fixed the ugly code with more ugly code

--- index.js
+++ index.js
@@ -3,62 +3,43 @@ const MAX_TB = 180
 var RECHARGE_INTERVAL = 0
 var FULL_RECHARGE_INTERVAL = 0
 
-function rechargeCalculation(){
-  clearInterval(RECHARGE_INTERVAL)
-  let tbInput = document.getElementById("input-tb").value
+function rechargeCalculation(element){
+
+  var inputTypeFlag = element.currentTarget.id == "input-tb"
+  clearInterval(inputTypeFlag ? RECHARGE_INTERVAL : FULL_RECHARGE_INTERVAL) 
+  let tbInput = element.currentTarget.value
   if(tbInput > MAX_TB){
     return
   }
 
-  var rechargeInSeconds = tbInput * TB_RECHARGE_IN_SEC
+  var rechargeInSeconds = inputTypeFlag ? tbInput * TB_RECHARGE_IN_SEC : (MAX_TB - tbInput) * TB_RECHARGE_IN_SEC
   let browserOffset = new Date().getTimezoneOffset() * 60 * 1000
 
-  updateRechargeDocument(rechargeInSeconds, browserOffset)
+  updateRechargeDocument(rechargeInSeconds, browserOffset, inputTypeFlag, element.currentTarget.parentNode)
   rechargeInSeconds -= 1
   if(rechargeInSeconds <= 0){
     return
   }
-  RECHARGE_INTERVAL = setInterval(function(){
-    if(rechargeInSeconds <= 0){
-      clearInterval(RECHARGE_INTERVAL)
+  var intervalId = setInterval(function(){
+    if(inputTypeFlag){
+      clearInterval()
     }
-    updateRechargeDocument(rechargeInSeconds, browserOffset)
+    if(rechargeInSeconds <= 0){
+      clearInterval(intervalId)
+    }
+    updateRechargeDocument(rechargeInSeconds, browserOffset, inputTypeFlag, element.currentTarget.parentNode)
     rechargeInSeconds -= 1
   }, 1000)
-}
 
-function fullRechargeCalculation(){
-  clearInterval(FULL_RECHARGE_INTERVAL)
-  let tbInput = document.getElementById("input-full-recharge").value
-  if(tbInput > MAX_TB){
-    return
+  if(inputTypeFlag){
+    RECHARGE_INTERVAL = intervalId
+  } else{
+    FULL_RECHARGE_INTERVAL = intervalId
   }
-
-  var fullRechargeInSeconds = (MAX_TB - tbInput) * TB_RECHARGE_IN_SEC
-  let browserOffset = new Date().getTimezoneOffset() * 60 * 1000
-
-  updateFullRechargeDocument(fullRechargeInSeconds, browserOffset)
-  fullRechargeInSeconds -= 1
-  if(fullRechargeInSeconds <= 0){
-    return
-  }
-
-  FULL_RECHARGE_INTERVAL = setInterval(function(){
-    if(fullRechargeInSeconds < 0){
-      clearInterval(FULL_RECHARGE_INTERVAL)
-    }
-    updateFullRechargeDocument(fullRechargeInSeconds, browserOffset)
-    fullRechargeInSeconds -= 1
-  }, 1000)
 }
 
-function updateRechargeDocument(rechargeInSeconds, browserOffset){
-  document.getElementById("recharge-time").innerHTML = new Date(rechargeInSeconds * 1000).toISOString().slice(11, 19);
-  document.getElementById("recharge-date").innerHTML = new Date(rechargeInSeconds * 1000 + Date.now() - browserOffset).toISOString().slice(11, 16) + " HS";
-}
-
-function updateFullRechargeDocument(rechargeInSeconds, browserOffset){
-  document.getElementById("page-title").innerHTML = "Full recharge in: " + new Date(rechargeInSeconds * 1000).toISOString().slice(11, 19);
-  document.getElementById("full-recharge-time").innerHTML = new Date(rechargeInSeconds * 1000).toISOString().slice(11, 19);
-  document.getElementById("full-recharge-date").innerHTML = new Date(rechargeInSeconds * 1000 + Date.now() - browserOffset).toISOString().slice(11, 16) + " HS";
+function updateRechargeDocument(rechargeInSeconds, browserOffset, inputTypeFlag, parentElement){
+  inputTypeFlag ? true : document.getElementById("page-title").innerHTML = "Full recharge in: " + new Date(rechargeInSeconds * 1000).toISOString().slice(11, 19);
+  document.getElementById(parentElement.children[4].id).innerHTML = new Date(rechargeInSeconds * 1000).toISOString().slice(11, 19);
+  document.getElementById(parentElement.children[6].id).innerHTML = new Date(rechargeInSeconds * 1000 + Date.now() - browserOffset).toISOString().slice(11, 16) + " HS";
 }
