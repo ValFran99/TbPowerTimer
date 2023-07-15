d48aa81c57de080d4eb3c4fcaebb4019eb557628
ValFran99 <72045238+ValFran99@users.noreply.github.com>
2023-07-15 01:27:49 -0300

    fix: some small fixes

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ var REFILL_INTERVAL = 0
 var FIRST_COUNTDOWN_INDEX = 3
 var SECOND_COUNTDOWN_INDEX = 5
 
-function showText(className){
-  var textElements = document.getElementsByClassName(className)
-  for(let i = 0; i < textElements.length; i++){
-    textElements[i].style.display = "block"
-  }
-}
+// Main function
 
 function rechargeCalculation(element){
   let tbInput = element.currentTarget.value
@@ -19,18 +14,24 @@ function rechargeCalculation(element){
   }
 
   var inputTypeFlag = element.currentTarget.id == "input-recharge"
+  var countdownType = inputTypeFlag ? "recharge" : "refill"
   if(inputTypeFlag){
     document.getElementById("recharge-text").innerHTML = tbInput + " trailblaze power will recharge in: "
   }
+
+  // clears whatever interval had before
   clearInterval(inputTypeFlag ? RECHARGE_INTERVAL : REFILL_INTERVAL) 
 
   var rechargeInSeconds = inputTypeFlag ? tbInput * TB_RECHARGE_IN_SEC : (MAX_TB - tbInput) * TB_RECHARGE_IN_SEC
+
+  // Shows the time of the refill/recharge
   let browserOffset = new Date().getTimezoneOffset() * 60 * 1000
+  document.getElementById(countdownType + "-date").innerHTML = new Date(rechargeInSeconds * 1000 + Date.now() - browserOffset).toISOString().slice(11, 16) + " HS";
 
   showText(inputTypeFlag ? "toggle-recharge" : "toggle-refill")
-  updateRechargeDocument(rechargeInSeconds, browserOffset, inputTypeFlag, element.currentTarget.parentNode.parentNode)
-
+  updateRechargeDocument(rechargeInSeconds, inputTypeFlag, countdownType)
   rechargeInSeconds -= 1
+
   if(rechargeInSeconds <= 0){
     return
   }
@@ -43,7 +44,7 @@ function rechargeCalculation(element){
       let audio = new Audio("alarm_sound.mp3")
       audio.play()
     }
-    updateRechargeDocument(rechargeInSeconds, browserOffset, inputTypeFlag, element.currentTarget.parentNode.parentNode)
+    updateRechargeDocument(rechargeInSeconds, inputTypeFlag, countdownType)
     rechargeInSeconds -= 1
   }, 1000)
 
@@ -54,12 +55,12 @@ function rechargeCalculation(element){
   }
 }
 
-function updateRechargeDocument(rechargeInSeconds, browserOffset, inputTypeFlag, parentElement){
+// Updates the countdown timers
+function updateRechargeDocument(rechargeInSeconds, inputTypeFlag, countdownType){
   let countdownString = new Date(rechargeInSeconds * 1000).toISOString().slice(11, 19)
   inputTypeFlag ? true : document.getElementById("page-title").innerHTML = "Full recharge in: " + countdownString;
 
-  document.getElementById(parentElement.children[FIRST_COUNTDOWN_INDEX].id).innerHTML = prettyCountdownFormat(countdownString);
-  document.getElementById(parentElement.children[SECOND_COUNTDOWN_INDEX].id).innerHTML = new Date(rechargeInSeconds * 1000 + Date.now() - browserOffset).toISOString().slice(11, 16) + " HS";
+  document.getElementById(countdownType + "-time").innerHTML = prettyCountdownFormat(countdownString);
 }
 
 function prettyCountdownFormat(countdownString){
@@ -69,26 +70,32 @@ function prettyCountdownFormat(countdownString){
   return countdownHour + countdownMinute + countdownSecond
 }
 
+function showText(className){
+  var textElements = document.getElementsByClassName(className)
+  for(let i = 0; i < textElements.length; i++){
+    textElements[i].style.display = "block"
+  }
+}
+
 // for the toggle switch
 
 function toggleSwitch(){
-  var circle = document.getElementsByClassName("inner-circle")[0]
-  var toggleButton = document.getElementsByClassName("toggle-button")[0]
-  var toggleRecharge = document.getElementById("recharge-calc")
-  var toggleRefill = document.getElementById("refill-calc")
-  var leftPosition = circle.style.left
+  let circle = document.getElementsByClassName("inner-circle")[0]
+  let toggleButton = document.getElementsByClassName("toggle-button")[0]
+  let toggleRecharge = document.getElementById("recharge-calc")
+  let toggleRefill = document.getElementById("refill-calc")
+  let leftPosition = circle.style.left
 
-  if(leftPosition == "50%"){
+  if(leftPosition == "51%"){
     circle.style.left = "5%"
     toggleButton.style.backgroundColor = "white"
     toggleRecharge.style.display = "none"
     toggleRefill.style.display = "block"
 
   } else{
-    circle.style.left = "50%"
+    circle.style.left = "51%"
     toggleButton.style.backgroundColor = "#007bff"
     toggleRecharge.style.display = "block"
     toggleRefill.style.display = "none"
-
   }
 }
